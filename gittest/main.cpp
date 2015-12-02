611e64b0a2acc073c23b2f9d1c2d2e063690c647
BLUE <blue@BLUE-no-MacBook-Pro.local>
2015-12-02 11:38:50 +0900

    Initial Commit

---
+++ gittest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by BLUE on 2015/12/02.
+//  Copyright (c) 2015年 noname. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
