7f547be52f9e87164b9b1a7e5230b5fe18efdcea
sasaka <sasakanaoki@gmail.com>
2015-12-03 13:09:47 +0900

    テストヘッダー追加 print() 関数のみ

---
+++ gittest/Test.h
@@ -0,0 +1,20 @@
+//
+//  Test.h
+//  gittest
+//
+//  Created by BLUE on 2015/12/03.
+//  Copyright (c) 2015年 noname. All rights reserved.
+//
+
+#ifndef __gittest__Test__
+#define __gittest__Test__
+
+#include <iostream>
+
+class Test
+{
+public:
+    void print();
+};
+
+#endif /* defined(__gittest__Test__) */
