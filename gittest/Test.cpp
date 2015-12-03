2bceddd23122ae8f8cc110a75fd949e99178337c
sasaka <sasakanaoki@gmail.com>
2015-12-03 13:10:17 +0900

    print() 関数の定義

---
+++ gittest/Test.cpp
@@ -0,0 +1,14 @@
+//
+//  Test.cpp
+//  gittest
+//
+//  Created by BLUE on 2015/12/03.
+//  Copyright (c) 2015年 noname. All rights reserved.
+//
+
+#include "Test.h"
+
+void Test::print()
+{
+    printf("テスト");
+}
